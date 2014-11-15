6449f54b6ccbd21a41fb9dcf112337306263681a
Marc Streeter <marcs@codero.com>
2014-11-15 17:42:12 -0600

    cleanup

--- api/config/shared.py
+++ api/config/shared.py
@@ -1,11 +1,11 @@
 # interwebs
 api_host = "localhost"
-api_url_prefix = "/v1"
+api_url_prefix = "/api/v1"
 
 # logging
 api_log_root_name = "vup."
 api_log_format = '%(asctime)s [%(process)d] [%(levelname)s] [%(name)s] %(filename)s:%(lineno)d - %(message)s'
-api_log_debug = '/var/log/api/vup-debug.log'
+api_log_debug = '/var/log/vup-debug.log'
 
 # db / cache
 redis_host = "localhost"
