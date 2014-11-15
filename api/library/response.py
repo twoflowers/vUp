a8d24ff5dae7e6a2162c0322c843966487c5deac
Marc Streeter <marcs@codero.com>
2014-11-15 15:53:20 -0600

    improved jsonify

--- api/library/response.py
+++ api/library/response.py
@@ -1,6 +1,15 @@
 # third party
 from flask import jsonify
 
-def jsonified(item):
-    return jsonify(data=[item] if isinstance(item, dict) else item)
+def jsonified(data):
+    resp = {"data": None, "status": 200, "success": True}
+    if isinstance(data, Exception):  # give generic exception response
+        resp['data'] = "{m} ({t})".format(m=data.message, t=str(type(data))[7:-2])
+        resp['status'] = 500
+        resp['success'] = False
+
+    if isinstance(data, (tuple, dict, list, str, int, long, unicode)):
+        resp['data'] = data
+
+    return jsonify(data=resp)
 
