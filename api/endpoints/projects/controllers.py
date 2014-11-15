c6ac8961cad45ea6ebd28d2e85400f3eec1e2e0e
Marc Streeter <marcs@codero.com>
2014-11-15 15:54:54 -0600

    updated

--- api/endpoints/projects/controllers.py
+++ api/endpoints/projects/controllers.py
@@ -5,10 +5,11 @@ import logging
 from api.config import shared_config
 
 # 3rd Party
-from flask import Blueprint, jsonify
+from flask import Blueprint
 
 # Shared
-
+from library import db
+from library.response import jsonified
 
 projects_blueprint = Blueprint('projects', __name__, url_prefix=shared_config.api_url_prefix + '/projects')
 logger = logging.getLogger(shared_config.api_log_root_name + __name__)
@@ -16,4 +17,5 @@ logger = logging.getLogger(shared_config.api_log_root_name + __name__)
 
 @projects_blueprint.route('/', methods=['GET'])
 def update():
-    return jsonify(data=["update is coming, you gave me"])
+    keys = db.keys()
+    return jsonified(data="update is coming, you gave me")
--- api/library/response.py
+++ api/library/response.py
@@ -1,15 +1,17 @@
 # third party
 from flask import jsonify
 
-def jsonified(data):
-    resp = {"data": None, "status": 200, "success": True}
+def jsonified(data, code=None):
+    resp = {"data": None, "status": None, "success": None}
     if isinstance(data, Exception):  # give generic exception response
         resp['data'] = "{m} ({t})".format(m=data.message, t=str(type(data))[7:-2])
-        resp['status'] = 500
+        resp['status'] = code or 500
         resp['success'] = False
 
     if isinstance(data, (tuple, dict, list, str, int, long, unicode)):
         resp['data'] = data
+        resp['status'] = code or 200
+        resp['success'] = True
 
     return jsonify(data=resp)
 
