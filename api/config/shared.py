7ccff8b1c1f6fe069a6239445febd42b3c89dd16
Marc Streeter <marcs@codero.com>
2014-11-15 16:40:27 -0600

    updated logging

--- api/config/shared.py
+++ api/config/shared.py
@@ -4,7 +4,7 @@ api_url_prefix = "/v1"
 
 # logging
 api_log_root_name = "vup."
-api_log_format = '%(asctime)s [' + api_host + '] [%(process)d] [%(levelname)s] [%(name)s] %(filename)s:%(lineno)d - %(message)s'
+api_log_format = '%(asctime)s [%(process)d] [%(levelname)s] [%(name)s] %(filename)s:%(lineno)d - %(message)s'
 
 # db / cache
 redis_host = "localhost"
