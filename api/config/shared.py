06742ca27d61b11e4dd4ec66fece7e84d3431dc0
Marc Streeter <marcs@codero.com>
2014-11-15 16:34:43 -0600

    added logging goodness

--- api/config/shared.py
+++ api/config/shared.py
@@ -1,3 +1,10 @@
-api_log_root_name = "vup."
+# interwebs
+api_host = "localhost"
 api_url_prefix = "/v1"
+
+# logging
+api_log_root_name = "vup."
+api_log_format = '%(asctime)s [' + api_host + '] [%(process)d] [%(levelname)s] [%(name)s] %(filename)s:%(lineno)d - %(message)s'
+
+# db / cache
 redis_host = "localhost"
