9c304fcb8399b399c6a7aaa84a98625e99adf6e7
Marc Streeter <marcs@codero.com>
2014-11-15 16:47:14 -0600

    included logger location.... duh

--- api/config/shared.py
+++ api/config/shared.py
@@ -5,6 +5,7 @@ api_url_prefix = "/v1"
 # logging
 api_log_root_name = "vup."
 api_log_format = '%(asctime)s [%(process)d] [%(levelname)s] [%(name)s] %(filename)s:%(lineno)d - %(message)s'
+api_log_debug = '/var/log/api/vup-debug.log'
 
 # db / cache
 redis_host = "localhost"
