05c9a70be840244ccba4ce86b05a50b598155377
Marc Streeter <marcs@codero.com>
2014-11-15 18:16:34 -0600

    added error

--- api/library/errors.py
+++ api/library/errors.py
@@ -1,12 +1,32 @@
+# builtin
+import logging
+
+# config
+from config import shared_config
+
+logger = logging.getLogger(shared_config.api_log_root_name + __name__)
+
 class Errors(Exception):
+
     _message = "Whoops! You found a spot that's not working properly"
     _status_code = 400
+    _log_level = logging.WARN
 
-    def __init__(self, message=None, status_code=None, payload=None):
+    def __init__(self, message=None, status_code=None, payload=None, log_level=None):
         Exception.__init__(self)
         self.message = message
         self.status = status_code
         self.payload = payload
+        self.log_level = log_level
+        logger.log(level=self.log, msg=self.message)
+
+    @property
+    def log(self):
+        return self._log_level
+
+    @log.setter
+    def log(self, value):
+        self._log_level = value or logging.DEBUG
 
     @property
     def message(self):
@@ -29,5 +49,10 @@ class Errors(Exception):
 
 
 class NotFound(Errors):
-    _message = "The thing that you though you were looking for is not found in the place you thought it was..."
+    _message = "The thing that you thought you were looking for is not found in the place you'd wish it was..."
     _status_code = 404
+
+class Unhandled(Errors):
+    _message = "Oops! You found a mistake!"
+    _status_code = 500
+    _log_level = logging.ERROR
