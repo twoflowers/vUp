9c606701210379db6e1e7bd1ee082762bd903812
Marc Streeter <marcs@codero.com>
2014-11-15 22:39:17 -0600

    added more exception types

--- api/library/exc.py
+++ api/library/exc.py
@@ -38,8 +38,21 @@ class Errors(Exception):
         return {"message": self.message}
 
 
-
-class SystemError(Errors):
+class SystemErrors(Errors):
     _message = "Unhandled error"
     _log_level = logging.ERROR
 
+class SystemInvalid(SystemErrors):
+    _message = "System reached an invalid state under incorrect assumptions"
+
+class SystemNotFound(SystemErrors):
+    _message = "System thought something should be here and it's not"
+
+class UserErrors(Errors):
+    _message = "User made a mistake here"
+
+class UserInvalidUsage(UserErrors):
+    _message = "User you need to review the documentation ... you're not using the API correctly"
+
+class UserNotFound(UserErrors):
+    _message = "User thought there was something here.... there is not"
