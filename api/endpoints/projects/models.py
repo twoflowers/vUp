2724a4d02f80cebaccc9b433139f0c41cf1eaf5d
Marc Streeter <marcs@codero.com>
2014-11-16 01:05:59 -0600

    made so that project_id's  can't be overridden

--- api/endpoints/projects/models.py
+++ api/endpoints/projects/models.py
@@ -14,23 +14,28 @@ logger = logging.getLogger(shared_config.api_log_root_name + __name__)
 
 
 # helpers
-def proj_id(project_id):
+def proj_key(project_id):
     return "projects:project:{id}".format(id=project_id)
 
 
 def proj_exists(project_id):
-    proj_key = proj_id(project_id)
-    logger.debug("checking if proj key {p}".format(p=proj_key))
-    return db.pipe.exists(name=proj_key).execute()[0]
+    project_key = proj_key(project_id)
+    logger.debug("checking if proj key {p}".format(p=project_key))
+    result = db.pipe.exists(name=project_key).execute()[0]
+    logger.debug("retrieved result {r}".format(r=result))
+    return True if result else None
 
 
 # endpoint gate
 def project_create(name, containers, version):
     project_id = str(int(time()))
 
+    if proj_exists(project_id):
+        project_id = str(int(time()) + 1)
+
     try:
         project = {"id": project_id, "name": name, "version": version, "containers": containers}
-        db.pipe.set(name=proj_id(project_id), value=json.dumps(project)).execute()
+        db.pipe.set(name=proj_key(project_id), value=json.dumps(project)).execute()
 
     except Exception as e:
         logger.error("failed to create new project because %s" % e, exc_info=True)
@@ -43,33 +48,27 @@ def project_create(name, containers, version):
 def project_listing(project_id=None):
     logger.debug("entered listing({args})".format(args=locals()))
 
-    if project_id and not proj_exists(project_id):
-        raise exc.UserNotFound("no project with id {i}".format(id=project_id))
-
-    try:  # pull all projects
-        project_names = db.keys("projects:project:*") or []
-        logger.debug("returning list of all projects: {p}".format(p=project_names))
-        for project in project_names:
-            db.pipe.get(name=project["name"])
-
-    except Exception as e:
-        logger.error("failed to pull projects because %s" % e, exc_info=True)
-        raise exc.SystemInvalid()
-
-    projects = [json.loads(project) for project in db.pipe.execute()]
-    projects = projects if not project_id else [proj for proj in projects]
-
-    if project_id:  # pull only one project
-        projects = [project for project in projects if project["id"] == int(project_id)]
-        logger.debug("project {n} exists, pulling detail".format(n=project_id))
-        if projects:
-            return projects
+    if project_id:
+        if not proj_exists(project_id):
+            raise exc.UserNotFound("no project with id {i}".format(id=project_id))
         else:
-            raise exc.UserNotFound("no project with id {i}".format(i=project_id))
+            results = db.pipe.get(name=proj_key(project_id)).execute()
+            logger.debug("retrieved results {r}".format(r=results))
+    else:
+        try:  # pull all projects
+            project_names = db.keys("projects:project:*") or []
+            logger.debug("returning list of all projects: {p}".format(p=project_names))
+            for project in project_names:
+                db.pipe.get(name=project["name"])
 
-    else:  # return list of details
-        return projects
+            results = db.pipe.execute()
+        except Exception as e:
+            logger.error("failed to pull projects because %s" % e, exc_info=True)
+            raise exc.SystemInvalid()
 
+    projects = [json.loads(project) for project in results]
+    logger.debug("returning project(s) {p}".format(p=projects))
+    return projects
 
 def update(listing_id):
 
