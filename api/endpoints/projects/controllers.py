9fbfc8c52213475a5bdbd3f3ec4b4e12f94002fc
Marc Streeter <marcs@codero.com>
2014-11-15 18:17:17 -0600

    added error

--- api/endpoints/projects/controllers.py
+++ api/endpoints/projects/controllers.py
@@ -10,6 +10,7 @@ from flask import Blueprint
 # shared
 from library import db
 from library.response import jsonified
+from library import errors
 
 projects_blueprint = Blueprint('projects', __name__, url_prefix=shared_config.api_url_prefix + '/projects')
 logger = logging.getLogger(shared_config.api_log_root_name + __name__)
@@ -17,5 +18,10 @@ logger = logging.getLogger(shared_config.api_log_root_name + __name__)
 
 @projects_blueprint.route('/<project_id>', methods=['GET'])
 def listing(project_id):
-    keys = db.keys()
-    return jsonified(data="keys are currently {k}, you wanted to view {p}".format(k=keys, p=project_id))
+    try:
+        keys = db.keys()
+        return jsonified(data="keys are currently {k}, you wanted to view {p}".format(k=keys, p=project_id))
+
+    except Exception as e:
+        logger.error("unhandled error {e}".format(e=e), exc_info=True)
+        raise errors.Unhandled()
